4da79f8fdda439d5d045a1d3e830c7558e618f9a
Christopher D. Long <galizur@gmail.com>
2017-02-16 16:27:59 -0500

    Home advantage.

--- ncaa_pbp/sql/home_advantage.sql
+++ ncaa_pbp/sql/home_advantage.sql
@@ -1,6 +1,6 @@
 select
 avg(tp)::numeric(4,1) as ppg,
-(avg(tp*f.exp_factor^2)-avg(tp))::numeric(4,2) as ha
+(avg(tp*f.exp_factor)-avg(tp))::numeric(4,2) as ha
 from (
 select
 game_id,sum(ps) as tp
@@ -11,6 +11,7 @@ p.game_id,
 unnest(p.period_scores) as ps
 from ncaa_pbp.periods p
 join ncaa_pbp.team_schedules ts
+--on (ts.game_id,ts.team_id)=(p.game_id,p.team_id)
 on (ts.game_id)=(p.game_id)
 where
     p.team_id is not null
